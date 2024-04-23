de9af178469d566ba906fa167cfc89896359128c
Prajwal B I <prajwalnbv@gmail.com>
2024-04-23 19:59:37 +0530

    production env and dynamic port

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ import ViteExpress from "vite-express";
 
 const app = express();
 
-// ViteExpress.config({ mode: "production" })
+const port = process.env.PORT || 3000;
+ViteExpress.config({ mode: "production" })
 app.get("/message", (_, res) => res.send("Hello from express!"));
 
-ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
+ViteExpress.listen(app, port, () => console.log("Server is listening..."));
